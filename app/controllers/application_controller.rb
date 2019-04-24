82b20c4a3b41eb44f3abe905c51b914bd4d98e84
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-24 17:27:21 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,44 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get "/" do
+    redirect to "/recipes"
+  end
+
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get "/recipes/new" do
+    erb :new
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params[:recipe])
+    redirect to "/recipes/#{ @recipe.id }"
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    # params.delete("_method")
+    @recipe.update(params[:recipe])
+    redirect to "/recipes/#{ @recipe.id }"
+  end
+
+  delete "/recipes/:id" do
+    Recipe.destroy(params[:id])
+    redirect to "/recipes"
+  end
+
 end
